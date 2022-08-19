ac4de5342076380560fbcf4a9006372f85d92762
hapk <96176933+hapk-dls@users.noreply.github.com>
2022-08-19 23:34:45 +0700

    update map

--- js/app.js
+++ js/app.js
@@ -69,5 +69,22 @@ $(function () {
 
   // And pass it to panzoom
 
-  panzoom(element);
+  panzoom(element, {
+
+    onDoubleClick: function (e) {
+
+      // `e` - is current double click event.
+
+      return false; // tells the library to not preventDefault, and not stop propagation
+    },
+
+    onTouch: function (e) {
+
+      // `e` - is current touch event.
+
+
+      return false; // tells the library to not preventDefault.
+    }
+
+  });
 });
